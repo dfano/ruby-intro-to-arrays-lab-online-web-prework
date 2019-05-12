4e092beab86e8ff1b1c8a70f3e883925551887ce
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-12 15:03:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,7 +9,7 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array.first
+  array[0]
 end
 
 def third_element(array)
@@ -19,3 +19,7 @@ end
 def last_element(array)
   array.last
 end
+
+def first_element_with_array_methods(array)
+  array.first
+end 
